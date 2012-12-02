143232a19ef2a1cafe870fb1be50d3bdbbefbecc
Rich Casesa <richcasesa@gmail.com>
2012-12-02 14:52:49 -0500

    fix main.js

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ var app = {
     },
 	
 	showAlert: function (message, title) {
-	    if (navigator.notification) {
-	        navigator.notification.alert(message, null, title, 'OK');
-	    } else {
-	        alert(title ? (title + ": " + message) : message);
-	    }
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
 	},
 
 /*
