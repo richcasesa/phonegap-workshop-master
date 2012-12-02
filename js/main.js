d7f5f4f495ddbdda2c365e583fddd3bd7e75165f
Rich Casesa <richcasesa@gmail.com>
2012-12-02 14:51:05 -0500

    updated alert

--- js/main.js
+++ js/main.js
@@ -12,12 +12,29 @@ var app = {
             }
         });
     },
+	
+	showAlert: function (message, title) {
+	    if (navigator.notification) {
+	        navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+	        alert(title ? (title + ": " + message) : message);
+	    }
+	},
 
+/*
     initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+*/
 
+	initialize: function() {
+	    var self = this;
+	    this.store = new MemoryStore(function() {
+	        self.showAlert('Store Initialized', 'Info');
+	    });
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}	
 };
 
 app.initialize();
