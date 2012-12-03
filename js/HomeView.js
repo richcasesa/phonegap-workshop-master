daf17275889f936662b0e07fd5b12aa45511be70
Rich Casesa <richcasesa@gmail.com>
2012-12-02 21:42:37 -0500

    modified scrolling

--- js/HomeView.js
+++ js/HomeView.js
@@ -8,7 +8,7 @@ var HomeView = function(store) {
             self.iscroll.refresh();
         } else {
             console.log('New iScroll');
-            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: true });
         }		
 	    });
 	};
