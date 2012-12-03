f1274f7ef7474fd89458593e521ba523705e27c9
Rich Casesa <richcasesa@gmail.com>
2012-12-02 22:07:06 -0500

    removed iScroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -3,13 +3,13 @@ var HomeView = function(store) {
     this.findByName = function() {
 		store.findByName($('.search-key').val(), function(employees) {
 	        $('.employee-list').html(HomeView.liTemplate(employees));
-		if (self.iscroll) {
-            console.log('Refresh iScroll');
-            self.iscroll.refresh();
-        } else {
-            console.log('New iScroll');
-            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: true });
-        }		
+		// if (self.iscroll) {
+//             console.log('Refresh iScroll');
+//             self.iscroll.refresh();
+//         } else {
+//             console.log('New iScroll');
+//             self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: true });
+//         }		
 	    });
 	};
 
