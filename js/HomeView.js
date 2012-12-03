ce9df05c3f55c9c89544ae2433dca7a89361acd6
Rich Casesa <richcasesa@gmail.com>
2012-12-02 21:37:44 -0500

    added iScroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -3,6 +3,13 @@ var HomeView = function(store) {
     this.findByName = function() {
 		store.findByName($('.search-key').val(), function(employees) {
 	        $('.employee-list').html(HomeView.liTemplate(employees));
+		if (self.iscroll) {
+            console.log('Refresh iScroll');
+            self.iscroll.refresh();
+        } else {
+            console.log('New iScroll');
+            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+        }		
 	    });
 	};
 
