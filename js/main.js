b72d8dbd0826e993c8d5bbc5ed2983083835bc2f
Rich Casesa <richcasesa@gmail.com>
2012-12-03 19:48:11 -0500

    fixed bad chars

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 var app = {
 
 	registerEvents: function() {
-	    var self = this;
-	    // Check of browser supports touch events...
-	    if (document.documentElement.hasOwnProperty('ontouchstart')) {
-	        // ... if yes: register touch event listener to change the "selected" state of the item
-	        $('body').on('touchstart', 'a', function(event) {
-	            $(event.target).addClass('tappable-active');
-	        });
-	        $('body').on('touchend', 'a', function(event) {
-	            $(event.target).removeClass('tappable-active');
-	        });
-	    } else {
-	        // ... if not: register mouse events instead
-	        $('body').on('mousedown', 'a', function(event) {
-	            $(event.target).addClass('tappable-active');
-	        });
-	        $('body').on('mouseup', 'a', function(event) {
-	            $(event.target).removeClass('tappable-active');
-	        });
-	    }
+	    var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+        });
+    }
 	},
 
 	showAlert: function (message, title) {
@@ -27,16 +27,16 @@ var app = {
 			navigator.notification.alert(message, null, title, 'OK');
 		} else {
 			alert(title ? (title + ": " + message) : message);
-		};
+		}
 	},
 	
 	initialize: function() {
-	    var self = this;
+        var self = this;
 //		this.homeTpl = Handlebars.compile($("#home-tpl").html());
 //		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-	    this.store = new MemoryStore(function() {
-	        $('body').html(new HomeView(self.store).render().el);
-	    });
+        this.store = new WebSqlStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
+        });
 		self.registerEvents();
 	}
 };
